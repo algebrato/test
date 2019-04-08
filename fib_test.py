b54af2a4811b639ec81462cab919c45e176e6fb5
Stefano Mandelli <linuxfree2@gmail.com>
2019-04-08 14:31:16 +0200

    Add fib_test
    I have added a fib_test that I can use with pytest for checkt some specific
    return of fib function. This is performed with `pytest`

---
+++ fib_test.py
@@ -0,0 +1,16 @@
+from fib import fib
+
+# Funny for testing pytest (the assert function and test some specific return that you know)
+def test_fib():
+    assert fib(0) == 1
+    assert fib(1) == 1
+    assert fib(2) == 2
+    assert fib(3) == 3
+    assert fib(4) == 5
+
+
+
+
+
+
+
